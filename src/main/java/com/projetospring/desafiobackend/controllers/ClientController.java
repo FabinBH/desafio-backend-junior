4dfa2f932ace40ccf685ec7673b43ad2bc56715e
Fabio Ferreira <fabiodepaulo2008@gmail.com>
2024-07-21 10:02:49 -0300

    finalizando o client controller

--- src/main/java/com/projetospring/desafiobackend/controllers/ClientController.java
+++ src/main/java/com/projetospring/desafiobackend/controllers/ClientController.java
@@ -1,14 +1,12 @@
 package com.projetospring.desafiobackend.controllers;
 
 import com.projetospring.desafiobackend.entities.Client;
-import com.projetospring.desafiobackend.services.CarService;
 import com.projetospring.desafiobackend.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/clients")
@@ -17,11 +15,28 @@ public class ClientController {
     @Autowired
     private ClientService clientService;
 
-    @Autowired
-    private CarService carService;
-
     @GetMapping("/all")
     public List<Client> listarClientes() {
         return clientService.listarClientes();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Client> listarCliente(@PathVariable Long id) {
+        return clientService.listarClientePeloID(id);
+    }
+
+    @PostMapping
+    public Client cadastrarCliente(@RequestBody Client client) {
+        return clientService.cadastrarCliente(client);
+    }
+
+    @PutMapping("/{id}")
+    public Client editarCliente(@PathVariable Long id, @RequestBody Client client) {
+        return clientService.editarCliente(id, client);
+    }
+
+    @DeleteMapping("/{id}")
+    public void excluirCliente(@PathVariable Long id) {
+        clientService.excluirCliente(id);
     }
 }
