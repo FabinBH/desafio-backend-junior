9eac975f527e8ec2f7d8dcbf73c4f75d884614a8
Fabio Ferreira <fabiodepaulo2008@gmail.com>
2024-07-22 11:24:53 -0300

    mudança nno retorno dos métodos

--- src/main/java/com/projetospring/desafiobackend/controllers/CarController.java
+++ src/main/java/com/projetospring/desafiobackend/controllers/CarController.java
@@ -3,6 +3,7 @@ package com.projetospring.desafiobackend.controllers;
 import com.projetospring.desafiobackend.entities.Car;
 import com.projetospring.desafiobackend.services.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,22 +22,23 @@ public class CarController {
     }
 
     @GetMapping("/{id}")
-    public Optional<Car> obterCarro(@PathVariable Long id) {
-        return carService.listarCarroPeloID(id);
+    public ResponseEntity<Car> obterCarro(@PathVariable Long id) {
+        return carService.listarCarroPeloID(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public Car cadastro(@RequestBody Car car) {
-        return carService.cadastrarCarro(car);
+    public ResponseEntity<Car> cadastro(@RequestBody Car car) {
+        return ResponseEntity.ok(carService.cadastrarCarro(car));
     }
 
     @PutMapping("/{id}")
-    public Car edit(@PathVariable Long id, @RequestBody Car car) {
-        return carService.editarCarro(id, car);
+    public ResponseEntity<Car> edit(@PathVariable Long id, @RequestBody Car car) {
+        return ResponseEntity.ok(carService.editarCarro(id, car));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         carService.excluirCarro(id);
+        return ResponseEntity.noContent().build();
     }
 }
